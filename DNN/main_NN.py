05120f920820de30ef90c88da881aa273da35a12
mazyaniv <mazya@post.bgu.ac.il>
2023-08-18 11:19:01 +0300

    shshl

--- DNN/main_NN.py
+++ DNN/main_NN.py
@@ -25,9 +25,9 @@ if __name__ == "__main__":
     for i in range(len(SNR_space)):
         for j in range(len(N_a)):
             my_parameters = prameters_class(N_a[j]+N_q[j],N_q[j], SNR_space[i], pram["snap"], pram["teta_range"],pram["D"],pram["C"]) #M=N_a+N_q
+
             if my_dict["Generate new data"]:
                 generate_data(my_parameters,train_prameters,file_path)
-
             data_file_path = file_path+'Data/'
             my_data = My_data(data_file_path,my_parameters)
 
