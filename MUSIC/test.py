b5bd127e3faf8a7cb1eb23b9801b2caa54b508f7
mazyaniv <mazya@post.bgu.ac.il>
2023-08-18 12:07:28 +0300

    shshl

--- MUSIC/test.py
+++ MUSIC/test.py
@@ -4,26 +4,29 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 if __name__ == "__main__":
-    SNR_space = np.linspace(-5, 25, 7)
-    N_a = [0,10,5]
-    N_q = [10,0,5]
+    #SNR_space = np.linspace(-15, 15, 6)
+    SNR = 0
+    snap_space = np.linspace(100, 1000, 7)
+    #snap = 400
+
+    N_a = [1,1,2,2]
+    N_q = [8,10,8,10]
     D = 2
     teta_range = [0, 60]
-    snap = 10
-    monte = 10
+    monte = 200
     C = 10  # Mask
-    Error = np.zeros((len(SNR_space), len(N_a)))
-    for i in range(len(SNR_space)):
+    Error = np.zeros((len(snap_space), len(N_a)))
+    for i in range(len(snap_space)):
         for j in range(len(N_a)):
-            my_parameters = prameters_class(N_a[j]+N_q[j],N_q[j],D,teta_range,SNR_space[i],snap,monte,C)
+            my_parameters = prameters_class(N_a[j]+N_q[j],N_q[j],D,teta_range,SNR,int(snap_space[i]),monte,C)
             Error[i,j] = music_algorithm(my_parameters)
 
     fig = plt.figure(figsize=(10, 6))
     for i in range(len(N_a)):
-        plt.plot(SNR_space, Error[:,i], label=f'Analog={N_a[i]}, Quantize={N_q[i]}')
-    plt.title(f"RMSE for snap={my_parameters.snap}, M={my_parameters.M}, D={my_parameters.D}")
+        plt.plot(snap_space, Error[:,i], label=f'Analog={N_a[i]}, Quantize={N_q[i]}')
+    plt.title(f"RMSE for SNR={my_parameters.SNR}, M={my_parameters.M}, D={my_parameters.D}, monte={my_parameters.monte}")
     plt.ylabel("RMSE (Deg.)")
-    plt.xlabel("SNR [dB]")
+    plt.xlabel("snapshots")
     plt.legend()
     plt.show()
 
