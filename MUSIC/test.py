5c1595b90a90af3667d1c544deffc6c2b0b193d1
mazyaniv <mazya@post.bgu.ac.il>
2023-08-18 18:01:07 +0300

    shshl

--- MUSIC/test.py
+++ MUSIC/test.py
@@ -4,29 +4,36 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 if __name__ == "__main__":
-    #SNR_space = np.linspace(-15, 15, 6)
-    SNR = 0
-    snap_space = np.linspace(100, 600, 5)
-    #snap = 400
+    SNR_space = np.linspace(-10, 10, 6)
+    #SNR = 0
+    #snap_space = np.linspace(100, 600, 5)
+    snap = 400
 
-    N_a = [1,1,2,2]
-    N_q = [8,10,8,10]
+    N_a = [0,8,5]
+    N_q = [10,2,5]
     D = 2
     teta_range = [0, 60]
     monte = 200
     C = 10  # Mask
-    Error = np.zeros((len(snap_space), len(N_a)))
-    for i in range(len(snap_space)):
+    Error1 = np.zeros((len(SNR_space), len(N_a)))
+    Error2 = np.zeros((len(SNR_space), len(N_a)))
+    Error3 = np.zeros((len(SNR_space), len(N_a)))
+    for i in range(len(SNR_space)):
         for j in range(len(N_a)):
-            my_parameters = prameters_class(N_a[j]+N_q[j],N_q[j],D,teta_range,SNR,int(snap_space[i]),monte,C)
-            Error[i,j] = music_algorithm(my_parameters)
+            my_parameters = prameters_class(N_a[j]+N_q[j],N_q[j],D,teta_range,SNR_space[i],snap,monte,C)
+            Error1[i,j] = music_algorithm(my_parameters)
+            Error2[i,j] = music_algorithm(my_parameters,1)
+            Error3[i,j] = music_algorithm(my_parameters,2)
 
     fig = plt.figure(figsize=(10, 6))
+    colors = ['b', 'g', 'orange']
     for i in range(len(N_a)):
-        plt.plot(snap_space, Error[:,i], label=f'Analog={N_a[i]}, Quantize={N_q[i]}')
-    plt.title(f"RMSE for SNR={my_parameters.SNR}, M={my_parameters.M}, D={my_parameters.D}, monte={my_parameters.monte}")
+        plt.plot(SNR_space, Error1[:,i],color = colors[i], label=f'Analog={N_a[i]}, Quantize={N_q[i]}')
+        plt.plot(SNR_space, Error2[:, i],color = colors[i],marker='o', label=f'Analog={N_a[i]}, Quantize={N_q[i]}, Sin recon.')
+        plt.plot(SNR_space, Error3[:, i],color = colors[i], marker='v',label=f'Analog={N_a[i]}, Quantize={N_q[i]}, Lin recon.')
+    plt.title(f"RMSE for snap={my_parameters.snapshot}, M={my_parameters.M}, D={my_parameters.D}, monte={my_parameters.monte}")
     plt.ylabel("RMSE (Deg.)")
-    plt.xlabel("snapshots")
+    plt.xlabel("SNR [dB]")
     plt.legend()
     plt.show()
 
