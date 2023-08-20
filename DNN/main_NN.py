c8937e559c545cea96d5f41eb1ce65395e288d32
mazyaniv <mazya@post.bgu.ac.il>
2023-08-20 11:48:42 +0300

    shshl

--- DNN/main_NN.py
+++ DNN/main_NN.py
@@ -7,15 +7,15 @@ import torch
 import numpy as np
 
 if __name__ == "__main__":
-    SNR_space = np.linspace(-5, 25, 8)[:-2]
+    SNR_space = np.linspace(-5, 25, 8)
     N_a = [0, 10, 5]
     N_q = [10, 0, 5]
     pram = {"snap":400, "teta_range":[0,60], "D":2, "C":10}
-    train_prameters = train_prameters(10000, 100, 100, 20, 0.001)
-    my_dict = {"device":"CPU",
-               "Generate new data": False,
-               "Train": False,
-               "Test": True,"Plot": True}
+    train_prameters = train_prameters(50000, 200, 100, 40, 0.0001)
+    my_dict = {"device":"Cuda",
+               "Generate new data": True,
+               "Train": True,
+               "Test": False,"Plot": False}
 # ======================================================================================================================
     if my_dict["device"] == "Cuda":
         file_path = '/home/mazya/DNN/'
